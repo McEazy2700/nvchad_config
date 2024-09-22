51ac22f1d3a774b29caf77ba2c6304eed11a80eb
Ezekiel Victor <codeepoch@gmail.com>
2024-09-22 17:44:42 +0100

    moving to astrovim

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.nu = true
+o.rnu = true
+o.cursorline = true
+o.wrap = false
